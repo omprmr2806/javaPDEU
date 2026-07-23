2b16c51744e37a30ba71da7436334fa856845e25
omprmr2806 <omprmr2806@gmail.com>
2026-07-23 12:31:55 +0530

    Add files via upload

---
+++ java/01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PrimeNumber{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner("System.in");
+        int n=sc.nextInt();
+        // int n=5;
+        for(int i=2; i<n; i++){
+        if(n%i==0)
+        {
+            System.out.println("Not prime");
+        }
+        else{
+            System.out.println("prime number");
+        }
+    }
+        // System.out.println("Hello,G10 Students");
+    }
+    sc.Close();
+}
